ec9172a381a18080364e4daad149876f6fdbe019
tabish-bp <tabish@bitspro.com>
2023-11-11 20:03:59 +0500

    forget_pass

--- scrap/users/urls.py
+++ scrap/users/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('register/', views.register_controller, name='register'),
     path('login/', views.login_controller, name='login'),
+    path('forgot-password/', views.forgot_password_controller, name='forgot-password'),
     path('logout/', views.logout_controller, name='logout'),
 ]
--- scrap/users/views.py
+++ scrap/users/views.py
@@ -1,35 +1,44 @@
 from django.shortcuts import redirect, render
 from .models import User
 from django.contrib.auth import authenticate, login, logout
+
 # Create your views here.
 
-def register_controller(request):
-    if request.method == 'POST':
-        firstname = request.POST.get('firstname')
-        lastname = request.POST.get('lastname')
-        email = request.POST.get('email')
-        password = request.POST.get('password')
 
-        user = User.objects.create_user(first_name=firstname, last_name=lastname, email=email, password=password)
+def register_controller(request):
+    if request.method == "POST":
+        firstname = request.POST.get("firstname")
+        lastname = request.POST.get("lastname")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        user = User.objects.create_user(
+            first_name=firstname, last_name=lastname, email=email, password=password
+        )
 
         user.save()
 
-        return render(request, 'users/register.html')
-    
-    return render(request, 'users/register.html')
+        return render(request, "users/register.html")
+
+    return render(request, "users/register.html")
+
 
 def login_controller(request):
-    if request.method == 'POST':
-        email = request.POST.get('email')
-        password = request.POST.get('password')
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
 
         user = authenticate(request, email=email, password=password)
 
         if user is not None:
             login(request, user)
-            return redirect('/')
-    return render(request, 'users/login.html')
+            return redirect("/")
+    return render(request, "users/login.html")
+
+
+def forgot_password_controller(request):
+    return render(request, "users/forgot_password.html")
 
 def logout_controller(request):
     logout(request)
-    return redirect('/')
+    return redirect("/")
