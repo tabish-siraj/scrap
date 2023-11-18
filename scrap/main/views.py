008b01888a2512e36e16fce2722b22aefc4f90a6
tabish-siraj <tabish1226@gmail.com>
2023-11-18 22:24:31 +0500

    as of things working for now

--- scrap/main/views.py
+++ scrap/main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-
+from users import views as users
 # Create your views here.
 
-def index(request):
+def index(request):    
+    if request.method == "POST" and 'contactus' in request.POST:
+        return render(request, 'main/contactus.html')
+
     return render(request, 'main/index.html')
